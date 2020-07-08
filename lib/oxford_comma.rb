b670f749dd3be4ab1afbaf4ce5eb8d17f76ce59f
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-08 17:35:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 def oxford_comma(array)
-  array = "kiwi"
-end
 
-def 
+  case array.length 
+end 	when 1
+  "#{array[0]}"
+when 2
+  array[0..1].join(" and ")
+else 
+  array[0...-1].join(", ") << ", and #{array[-1]}"
+end 
+end
